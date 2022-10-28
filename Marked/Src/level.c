026c2f93d0f0db60dc24d2b7ad08c6c9b1b8bdf9
xavchoa <76810223+xavchoa@users.noreply.github.com>
2022-10-28 15:25:47 +0800

    Player throwing projectile implemented
    implemented throwing mechanic without collision

--- Marked/Src/level.c
+++ Marked/Src/level.c
@@ -3,15 +3,16 @@
 #include "utils.h"
 #include "mainmenu.h"
 
-#define FALSE 0;
-#define TRUE 1;
+#define FALSE 0
+#define TRUE 1
 
 int windowWidth, windowHeight;
 float const gravity = 750.f;
 CP_BOOL isGrounded = FALSE;
 CP_BOOL rightPressed = FALSE;
 CP_BOOL leftPressed = FALSE;
-CP_BOOL isShooting = FALSE;
+CP_BOOL throwPressed = FALSE;
+CP_BOOL isThrowing = FALSE;
 
 typedef struct Velocity {
 	float x;
@@ -23,7 +24,7 @@ typedef struct Position {
 	float y;
 } Position;
 
-typedef struct Player{
+typedef struct Player {
 	float width;
 	float height;
 	Position pos;
@@ -34,6 +35,7 @@ typedef struct Player{
 typedef struct Projectile {
 	float width;
 	float height;
+	float range;
 	Position pos;
 	Velocity vel;
 } Projectile;
@@ -46,9 +48,9 @@ typedef struct Rect {
 } Rect;
 
 Player player;
-Projectile proj;
+Projectile projectile;
 
-CP_BOOL RectCollision(Rect *r1, Rect *r2) {
+CP_BOOL RectCollision(Rect* r1, Rect* r2) {
 	if (r1->x + r1->width >= r2->x && r1->x <= r2->x + r2->width &&
 		r1->y + r1->height >= r2->height && r1->y <= r2->y + r2->height) {
 		return TRUE;
@@ -56,42 +58,54 @@ CP_BOOL RectCollision(Rect *r1, Rect *r2) {
 	return FALSE;
 }
 
+void DrawProjectile(Projectile* proj) {
+	CP_Settings_Fill(CP_Color_Create(120, 0, 0, 255));
+	CP_Settings_RectMode(CP_POSITION_CENTER);
+	CP_Graphics_DrawRect(proj->pos.x, proj->pos.y, proj->width, proj->height);
+}
+
 void CreatePlatform(float x, float y, float width, float height) {
-	CP_Settings_Fill(CP_Color_Create(0,0,0,255));
+	CP_Settings_Fill(CP_Color_Create(0, 0, 0, 255));
 	CP_Settings_RectMode(CP_POSITION_CORNER);
 	CP_Graphics_DrawRect(x, y, width, height);
 
 	//platform collision detection
 	if (player.pos.y + player.height <= y && player.pos.y + player.height + player.vel.y * CP_System_GetDt() >= y &&
-		player.pos.x + player.width >= x && player.pos.x <= x+width) {
+		player.pos.x + player.width >= x && player.pos.x <= x + width) {
 		player.vel.y = 0.f;
 		isGrounded = TRUE;
 	}
 	if (player.pos.y <= y + height && player.pos.y - player.vel.y * CP_System_GetDt() >= y + height &&
 		player.pos.x + player.width >= x && player.pos.x <= x + width) {
-			player.vel.y = 0.f;
+		player.vel.y = 0.f;
 	}
 
-	if (player.pos.x + player.width >= x && player.pos.x <= x+ width &&
+	if (player.pos.x + player.width >= x && player.pos.x <= x + width &&
 		player.pos.y + player.height >= y && player.pos.y <= y + height &&
 		player.pos.x + player.width + player.vel.x * CP_System_GetDt() >= x && player.pos.x + player.vel.x * CP_System_GetDt() <= x + width) {
-			player.vel.x = 0.f;
+		player.vel.x = 0.f;
 	}
 }
 
-void DrawPlayer(Player *player) {
+
+void SetProjSpawn(float x, float y) {
+	if (isThrowing == 0) {
+		projectile.pos.x = x;
+		projectile.pos.y = y;
+		isThrowing = TRUE;
+	}
+}
+
+
+void DrawPlayer(Player* player) {
 	CP_Settings_Fill(player->color);
 	CP_Settings_RectMode(CP_POSITION_CORNER);
 	CP_Graphics_DrawRect(player->pos.x, player->pos.y, player->width, player->height);
-	CP_Settings_Fill(CP_Color_Create(255,255,255,255));
+	CP_Settings_Fill(CP_Color_Create(255, 255, 255, 255));
 
-} 
-
-void DrawProjectile(Projectile *proj) {
-	CP_Settings_Fill(CP_Color_Create(120, 0, 0, 255));
-	CP_Settings_RectMode(CP_POSITION_CENTER);
-	CP_Graphics_DrawRect(proj->pos.x, proj->pos.y, 20.f, 20.f);
 }
+
+
 
 void Level_Init()
 {
@@ -101,21 +115,24 @@ void Level_Init()
 	player.height = 50.f;
 	player.vel.x = 0.f;
 	player.vel.y = 500.f;
-	player.color = CP_Color_Create(255,255,255,255);
+	player.color = CP_Color_Create(255, 255, 255, 255);
 
-	proj.width = 20.f;
-	proj.height = 20.f;
+	projectile.pos.x = -100.f;
+	projectile.pos.y = -100.f;
+	projectile.width = 20.f;
+	projectile.height = 20.f;
+	projectile.vel.x = 7000.f;
+	projectile.range = 1000.f;
+
 	CP_System_SetWindowSize(windowWidth, windowHeight);
 
 }
 
 void Level_Update()
 {
-	proj.pos.x = player.pos.x + player.width + 10;
-	proj.pos.y = player.pos.y + player.height / 2;
 	CP_Graphics_ClearBackground(CP_Color_Create(240, 200, 200, 255));
 
-	CreatePlatform(500.f, windowHeight * 0.9, 5000.f,200.f);
+	CreatePlatform(500.f, windowHeight * 0.9, 5000.f, 200.f);
 	CreatePlatform(1000.f, windowHeight * 0.8, 100, 100.f);
 	CreatePlatform(200.f, windowHeight * 0.8, 100, 100.f);
 
@@ -123,7 +140,7 @@ void Level_Update()
 	player.pos.y += player.vel.y * CP_System_GetDt();
 	player.pos.x += player.vel.x * CP_System_GetDt();
 
-	if ((player.height + player.pos.y + player.vel.y * CP_System_GetDt())  <= windowHeight) {
+	if ((player.height + player.pos.y + player.vel.y * CP_System_GetDt()) <= windowHeight) {
 		player.vel.y += gravity * CP_System_GetDt();
 	}
 	else {
@@ -149,33 +166,45 @@ void Level_Update()
 	if (CP_Input_KeyReleased(KEY_D)) {
 		rightPressed = FALSE;
 	}
-	//shoot
+	//player throwing
 	if (CP_Input_KeyTriggered(KEY_X)) {
-		isShooting = !isShooting;
+		throwPressed = TRUE;
+	}
+	if (CP_Input_KeyReleased(KEY_X)) {
+		throwPressed = FALSE;
+	}
 
-	}
-	
-	if (isShooting) {
-		DrawProjectile(&proj);
-	}
-	
 	if (rightPressed) {
-		if(5000.f * CP_System_GetDt())
-		player.vel.x = 7000.f * CP_System_GetDt();
+		if (5000.f * CP_System_GetDt())
+			player.vel.x = 7000.f * CP_System_GetDt();
 	}
-	else if(leftPressed) {
+	else if (leftPressed) {
 		player.vel.x = -7000.f * CP_System_GetDt();
 	}
 	else {
 		player.vel.x = 0.f;
 	}
-	
+
 	DrawPlayer(&player);
 
+	if (isThrowing == TRUE) {
+		projectile.pos.x += 1000 * CP_System_GetDt();
+
+		if (projectile.pos.x > player.pos.x + projectile.range) {
+			isThrowing = FALSE;
+			projectile.pos.y = 1000; //makes projectile go offscreen
+
+		}
+	}
+	if (throwPressed) {
+		SetProjSpawn(player.pos.x + player.width, player.pos.y + player.height / 2);
+	}
 
 	if (CP_Input_KeyDown(KEY_Q)) {
 		CP_Engine_SetNextGameState(Main_Menu_Init, Main_Menu_Update, Main_Menu_Exit);
 	}
+
+	DrawProjectile(&projectile);
 }
 
 void Level_Exit()
