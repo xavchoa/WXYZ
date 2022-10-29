c3af2dd52e941f922dac44e55fdc3ca2763dee74
xavchoa <76810223+xavchoa@users.noreply.github.com>
2022-10-30 01:02:03 +0800

    GameOver added

--- Marked/Src/level.c
+++ Marked/Src/level.c
@@ -13,13 +13,20 @@ CP_BOOL rightPressed = FALSE;
 CP_BOOL leftPressed = FALSE;
 CP_BOOL throwPressed = FALSE;
 CP_BOOL isThrowing = FALSE;
+CP_BOOL gameOver = FALSE;
+
+typedef enum gameObjectType{
+	typePlayer, typeProjectile, typePlatform, typeEnemy
+} gameObjectType;
 
 typedef struct GameObject {
 	float width;
 	float height;
 	CP_Vector pos;
+	CP_BOOL isActive;
 	CP_BOOL hasCollider;
 	CP_BOOL isMarkable;
+	gameObjectType type;
 	void* extData;
 } GameObject;
 
@@ -28,6 +35,7 @@ typedef struct Player {
 	float height;
 	CP_Vector pos;
 	CP_Vector vel;
+	CP_Vector dir;
 	CP_Color color;
 } Player;
 
@@ -57,23 +65,53 @@ CP_BOOL RectCollision(Rect* r1, Rect* r2) {
 	return FALSE;
 }
 
-void PlayerInit(Player *player, CP_Vector p, CP_Vector v, float w, float h) {
-	player->pos.x = p.x;
-	player->pos.y = p.y;
-	player->width = w;
-	player->height = h;
-	player->vel.x = v.x;
-	player->vel.y = v.y;
+void PlayerInit(Player *player, float x, float y) {
+	player->pos.x = x;
+	player->pos.y = y;
+	player->width = 50.f;
+	player->height = 50.f;
+	player->vel.x = 0.f;
+	player->vel.y = 500.f;
 	player->color = CP_Color_Create(255, 255, 255, 255);
 }
 
-void ProjectileInit(Projectile *projectile, CP_Vector p, CP_Vector v, float w, float h, float r) {
-	projectile->pos.x = p.x;
-	projectile->pos.y = p.y;
-	projectile->width = w;
-	projectile->height = h;
-	projectile->vel.x = v.x;
-	projectile->range = r;
+void PlayerMovement() {
+	//player movement
+	if (CP_Input_KeyTriggered(KEY_UP) && isGrounded) {
+		player.vel.y -= 15000.f * CP_System_GetDt();
+		isGrounded = FALSE;
+	}
+	if (CP_Input_KeyDown(KEY_LEFT)) {
+		leftPressed = TRUE;
+	}
+	if (CP_Input_KeyReleased(KEY_LEFT)) {
+		leftPressed = FALSE;
+	}
+
+	if (CP_Input_KeyDown(KEY_RIGHT)) {
+		rightPressed = TRUE;
+	}
+	if (CP_Input_KeyReleased(KEY_RIGHT)) {
+		rightPressed = FALSE;
+	}
+	//player throwing
+	if (CP_Input_KeyTriggered(KEY_X)) {
+		throwPressed = TRUE;
+	}
+	if (CP_Input_KeyReleased(KEY_X)) {
+		throwPressed = FALSE;
+	}
+}
+
+
+void ProjectileInit(Projectile *projectile) {
+	projectile->pos.x = -100;
+	projectile->pos.y = -100;
+	projectile->width = 20;
+	projectile->height = 20;
+	projectile->vel.x = 7000;
+	projectile->vel.y = 0;
+	projectile->range = 1000;
 }
 void DrawProjectile(Projectile* proj) {
 	CP_Settings_Fill(CP_Color_Create(120, 0, 0, 255));
@@ -126,85 +164,70 @@ void DrawPlayer(Player* player) {
 
 void Level_Init()
 {
-	PlayerInit(&player, CP_Vector_Set(300,100), CP_Vector_Set(0,500), 50, 50);
-	ProjectileInit(&projectile, CP_Vector_Set(-100,-100), CP_Vector_Set(7000,1000), 20, 20, 1000);
-
 	CP_System_SetWindowSize(windowWidth, windowHeight);
+	PlayerInit(&player, 300.f,100.f);
+	ProjectileInit(&projectile);
 
 }
 
 void Level_Update()
 {
-	CP_Graphics_ClearBackground(CP_Color_Create(240, 200, 200, 255));
+	if (!gameOver) {
+		CP_Graphics_ClearBackground(CP_Color_Create(240, 200, 200, 255));
 
-	CreatePlatform(500.f, windowHeight * 0.9, 5000.f, 200.f);
-	CreatePlatform(1000.f, windowHeight * 0.8, 100, 100.f);
-	CreatePlatform(200.f, windowHeight * 0.8, 100, 100.f);
+		CreatePlatform(500.f, windowHeight * 0.9, 5000.f, 200.f);
+		CreatePlatform(1000.f, windowHeight * 0.8, 100, 100.f);
+		CreatePlatform(200.f, windowHeight * 0.8, 100, 100.f);
 
-	player.pos.y += player.vel.y * CP_System_GetDt(); 	//simulate gravity
-	player.pos.x += player.vel.x * CP_System_GetDt();
 
-	if ((player.height + player.pos.y + player.vel.y * CP_System_GetDt()) <= windowHeight+ player.height+50) {
-		player.vel.y += gravity * CP_System_GetDt();
-	}
-	else {
-		//GAMEOVER
-		player.vel.y = 0.f;
-		isGrounded = TRUE;
-	}
+		player.pos.y += player.vel.y * CP_System_GetDt(); 	//simulate gravity
+		player.pos.x += player.vel.x * CP_System_GetDt();
 
-	//player movement
-	if (CP_Input_KeyTriggered(KEY_W) && isGrounded) {
-		player.vel.y -= 15000.f * CP_System_GetDt();
-		isGrounded = FALSE;
-	}
-	if (CP_Input_KeyDown(KEY_A)) {
-		leftPressed = TRUE;
-	}
-	if (CP_Input_KeyReleased(KEY_A)) {
-		leftPressed = FALSE;
-	}
-
-	if (CP_Input_KeyDown(KEY_D)) {
-		rightPressed = TRUE;
-	}
-	if (CP_Input_KeyReleased(KEY_D)) {
-		rightPressed = FALSE;
-	}
-	//player throwing
-	if (CP_Input_KeyTriggered(KEY_X)) {
-		throwPressed = TRUE;
-	}
-	if (CP_Input_KeyReleased(KEY_X)) {
-		throwPressed = FALSE;
-	}
-
-	if (rightPressed) {
-		if (5000.f * CP_System_GetDt())
-			player.vel.x = 7000.f * CP_System_GetDt();
-	}
-	else if (leftPressed) {
-		player.vel.x = -7000.f * CP_System_GetDt();
-	}
-	else {
-		player.vel.x = 0.f;
-	}
-
-	DrawPlayer(&player);
-
-	if (isThrowing == TRUE) {
-		projectile.pos.x += 1000 * CP_System_GetDt();
-
-		if (projectile.pos.x > player.pos.x + projectile.range) {
-			isThrowing = FALSE;
-			projectile.pos.y = 1000; //makes projectile go offscreen
-
+		if ((player.height + player.pos.y + player.vel.y * CP_System_GetDt()) <= windowHeight) {
+			player.vel.y += gravity * CP_System_GetDt();
 		}
+		else {
+			gameOver = TRUE;
+			//player.vel.y = 0.f;
+			//isGrounded = TRUE;
+		}
+
+		PlayerMovement();
+
+		if (rightPressed) {
+			if (5000.f * CP_System_GetDt())
+				player.vel.x = 7000.f * CP_System_GetDt();
+		}
+		else if (leftPressed) {
+			player.vel.x = -7000.f * CP_System_GetDt();
+		}
+		else {
+			player.vel.x = 0.f;
+		}
+
+		DrawPlayer(&player);
+
+		if (isThrowing == TRUE) {
+			projectile.pos.x += 1000 * CP_System_GetDt();
+
+			if (projectile.pos.x > player.pos.x + projectile.range) {
+				isThrowing = FALSE;
+				projectile.pos.y = 1000; //makes projectile go offscreen
+
+			}
+		}
+		if (throwPressed) {
+			SetProjectileSpawn(player.pos.x + player.width, player.pos.y + player.height / 2);
+		}
+		DrawProjectile(&projectile);
 	}
-	if (throwPressed) {
-		SetProjectileSpawn(player.pos.x + player.width, player.pos.y + player.height / 2);
+	else {
+		//GAME OVER SCREEN
+		CP_Graphics_ClearBackground(CP_Color_Create(100, 50, 50, 5));
+		CreatePlatform(500.f, windowHeight * 0.9, 5000.f, 200.f);
+		CreatePlatform(1000.f, windowHeight * 0.8, 100, 100.f);
+		CreatePlatform(200.f, windowHeight * 0.8, 100, 100.f);
 	}
-	DrawProjectile(&projectile);
 
 	if (CP_Input_KeyDown(KEY_Q)) {
 		CP_Engine_SetNextGameState(Main_Menu_Init, Main_Menu_Update, Main_Menu_Exit);
