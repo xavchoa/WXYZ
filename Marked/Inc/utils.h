47f37355879c4ac1bdb5ff8b16af884c42d0326d
YUQXNG <c.yuqing@digipen.edu>
2022-11-25 17:45:30 +0800

    UPDATED

--- Marked/Inc/utils.h
+++ Marked/Inc/utils.h
@@ -1,2 +1,2 @@
 #pragma once
-int IsAreaClicked(float area_center_x, float area_center_y, float area_width, float area_height, float click_x, float click_y);
+int IsAreaClicked(float areaCenterX, float areaCenterY, float areaWidth, float areaHeight, float clickX, float clickY);
